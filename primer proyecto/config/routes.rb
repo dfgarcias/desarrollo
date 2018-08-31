b726138c50510dd3be33110ffcd180918c9a59ce
Juan Ramirez <ciscojuan@gmail.com>
2018-08-30 20:51:40 -0500

    clasedesesion

--- primer proyecto/config/routes.rb
+++ primer proyecto/config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
+
   get 'welcome/index'
  
   resources :posts
   resources :images
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index' 
+  root to: "home#index"
       
 end
